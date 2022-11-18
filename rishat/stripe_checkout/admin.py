37ece50b24139a3845e407af58276da473ad7538
Doppler344 <Doppler344@yandex.ru>
2022-11-18 13:49:11 +0300

    Created models and admin panel has been configured

--- rishat/stripe_checkout/admin.py
+++ rishat/stripe_checkout/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Item, Order, OrderSet, Tax, Discount
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'currency',)
+    list_display_links = ('name',)
+    search_fields = ('name', 'description',)
+
+
+admin.site.register(Item, ItemAdmin)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'date_of_order', 'status', 'currency', 'tax_id', 'discount_id',)
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderSet)
+
+
+class TaxAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'taxation_name', 'amount',)
+
+
+admin.site.register(Tax, TaxAdmin)
+
+
+class DiscountAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'discount_name', 'amount',)
+
+
+admin.site.register(Discount, DiscountAdmin)
--- rishat/stripe_checkout/models.py
+++ rishat/stripe_checkout/models.py
@@ -1,3 +1,73 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=128, verbose_name="Название", blank=False)
+    description = models.CharField(max_length=32, verbose_name="Описание", blank=True)
+    price = models.FloatField(validators=[MinValueValidator(float('0.0'))], verbose_name="Цена без скидки и налога",
+                              blank=False)
+    currency = models.CharField(verbose_name="Валюта", max_length=16, choices=[('USD', 'Доллар США'),
+                                                                               ('EUR', 'Евро'), ],
+                                blank=False)
+
+    def __str__(self):
+        return f'{self.pk} - {self.name} - {self.price} {self.currency}'
+
+    class Meta:
+        verbose_name_plural = 'Товары'
+        verbose_name = 'Товар'
+        ordering = ['-name']
+
+
+class Tax(models.Model):
+    taxation_name = models.CharField(max_length=128, verbose_name="Название", blank=False)
+    amount = models.FloatField(validators=[MinValueValidator(float('0.0')), MaxValueValidator(float('1.0'))],
+                               verbose_name="Налог",
+                               blank=False)
+
+    def __str__(self):
+        return f'{self.pk} - {self.taxation_name} - {self.amount}'
+
+    class Meta:
+        verbose_name_plural = 'Налоги'
+        verbose_name = 'Налог'
+        ordering = ['-taxation_name']
+
+
+class Discount(models.Model):
+    discount_name = models.CharField(max_length=128, verbose_name="Название", blank=False)
+    amount = models.FloatField(validators=[MinValueValidator(float('0.0')), MaxValueValidator(float('1.0'))],
+                               verbose_name="Скидка",
+                               blank=False)
+
+    def __str__(self):
+        return f'{self.pk} - {self.discount_name} - {self.amount}'
+
+    class Meta:
+        verbose_name_plural = 'Скидки'
+        verbose_name = 'Скидка'
+        ordering = ['-discount_name']
+
+
+class Order(models.Model):
+    date_of_order = models.DateField(verbose_name="Дата заказа", blank=True)
+    status = models.CharField(verbose_name="Статус", max_length=128, blank=True)
+    currency = models.CharField(verbose_name="Валюта", max_length=16, choices=[('USD', 'Доллар США'),
+                                                                               ('EUR', 'Евро'), ],
+                                blank=False)
+    tax_id = models.ForeignKey('Tax', on_delete=models.SET_NULL, null=True, blank=True)
+    discount_id = models.ForeignKey('Discount', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.pk} - {self.currency}'
+
+    class Meta:
+        verbose_name_plural = 'Заказы'
+        verbose_name = 'Заказ'
+
+
+class OrderSet(models.Model):
+    order_id = models.ForeignKey('Order', on_delete=models.CASCADE, blank=False)
+    item_id = models.ForeignKey('Item', on_delete=models.CASCADE, blank=False)
+    amount = models.PositiveIntegerField(verbose_name="Количество", blank=False)
